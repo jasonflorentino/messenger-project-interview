95c8dbf393a67c4a09e61c3e896ea9d32ebf7e35
Jason Florentino <jasonmflorentino@gmail.com>
2021-08-04 16:02:37 -0400

    Update the data structure for onlineUsers Change the implementation of onlineUsers to not use a JS array. Instead I've used a JS Map to allow faster lookup, storage, and deletion of user IDs. For simplicity, I've kept the lookup interface the same to save changing other files that already use this operation. Note that this change may be a performance gain over the previous implementation but it still in app memory and would not scale well for lots of users or over multiple instances. We should consider another solution if we need to accommodate such requirements.

--- server/onlineUsers.js
+++ server/onlineUsers.js
@@ -1,2 +1,13 @@
-const onlineUsers = [];
+const onlineUsers = {
+  store: new Map(),
+  includes: function(id) {
+    return this.store.has(id);
+  },
+  add: function(id) {
+    return this.store.set(id, true);
+  },
+  remove: function(id) {
+    return this.store.delete(id);
+  }
+};
 module.exports = onlineUsers;
