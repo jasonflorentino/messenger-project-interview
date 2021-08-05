f135f0bf1f7acddbe754befdcd6772d11c5ba00f
Jason Florentino <jasonmflorentino@gmail.com>
2021-08-05 16:10:01 -0400

    Remove gap between header and messages

--- client/src/components/ActiveChat/Header.js
+++ client/src/components/ActiveChat/Header.js
@@ -9,7 +9,6 @@ const useStyles = makeStyles((theme) => ({
     alignItems: "center",
     justifyContent: "space-between",
     minHeight: 89,
-    marginBottom: 34,
     boxShadow: "0 2px 20px 0 rgba(88,133,196,0.10)"
   },
   content: {
